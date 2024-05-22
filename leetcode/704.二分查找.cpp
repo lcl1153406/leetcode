7301b3772545704e83c9981189616a66894219ea
lcl <25844@DESKTOP-1R4199L>
2024-05-22 23:44:56 +0800

    二分查找

---
+++ leetcode/704.二分查找.cpp
@@ -0,0 +1,54 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem704.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=704 lang=cpp
+ *
+ * [704] 二分查找
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int size = nums.size();
+        if(size == 0)
+        {
+            return -1;
+        }
+        
+        int left = 0;
+        int right = size - 1;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
+
