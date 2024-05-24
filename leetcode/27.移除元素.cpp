cf9945817bd7261fd393419bb89efd9853cd81a6
lcl <25844@DESKTOP-1R4199L>
2024-05-25 00:13:42 +0800

    slow fast

---
+++ leetcode/27.移除元素.cpp
@@ -0,0 +1,36 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem27.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=27 lang=cpp
+ *
+ * [27] 移除元素
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++)
+        {
+            if (nums[fast] != val)
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+};
+// @lc code=end
+
